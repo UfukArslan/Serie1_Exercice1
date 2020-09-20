10699234e9a89c9fea4131777e7991fe3b661abb
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-09-20 21:30:51 +0200

    Copie des fichiers

---
+++ src/ComplexNumber.java
@@ -0,0 +1,99 @@
+	public class ComplexNumber {
+		
+		private double a;
+		private double b;
+//		static private String c;
+		
+		//constructors
+		
+		ComplexNumber() {}
+		
+		ComplexNumber(double x, double y)
+		{	
+			a = x;
+			b = y;
+		}
+		
+		
+		//setter/getter
+		
+		public double get_a()
+		{
+			return a;
+		}
+		
+		public double get_b()
+		{
+			return b;
+		}
+		
+		public String get_number()
+		{
+			//String text = String.valueOf(a) + ":" + String.valueOf(b);
+			String text = a + " : " + b;
+			return text;
+		}
+	
+	
+		public void set_a(double a)
+		{
+			this.a = a;
+		}
+		
+		public void set_b(double b)
+		{
+			this.b = b;
+		}
+		
+
+		
+		//functions
+		
+		public void add(ComplexNumber x)
+		{
+			this.a += x.get_a();
+			this.b += x.get_b();
+		}
+
+	
+		
+	}
+	
+	
+	
+//	public String get_number_toString()
+//	{
+//		//String text = String.valueOf(a) + ":" + String.valueOf(b);
+//		String text = a + " : " + b;
+//		return text;
+//	}
+//	
+//	
+//	public double[] get_number_asArray()
+//	{
+//		double[] A = {a, b};
+//		return A;
+//	}
+//	
+//	
+//	public static String get_c() {
+//		return c;
+//	}
+//
+//	public static void set_c(String c) {
+//		ComplexNumber.c = c;
+//	}
+	
+//	public static String getC() {
+//		return c;
+//	}
+//
+//	public static void setC(String c) {
+//		ComplexNumber.c = c;
+//	}
+
+	
+	
+	
+	
+
---
+++ src/Test.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		
+		ComplexNumber c1 = new ComplexNumber(1.0, 2.0);
+		ComplexNumber c2 = new ComplexNumber(3.0, 4.0); 
+		System.out.println("c1 = " + c1.get_number());
+		System.out.println("c2 = " + c2.get_number());
+
+	
+		
+	
+		
+//		ComplexNumber sum1 = c1.add(c2);
+//		System.out.println("sum1 = " + sum1);
+//		
+//		ComplexNumber sum2 = ComplexNumber.add(c1, c2);
+//		System.out.println("sum2 = " + sum2);
+//		
+//		ComplexNumber produit1 = c1.multiply(c2);
+//		System.out.println("produit1 = " + produit1);
+//		
+//		ComplexNumber produit2 = ComplexNumber.multiply(c1, c2);
+//		System.out.println("produit2 = " + produit2);
+//		System.out.println("Partie réelle de c1 = " + c1.real());
+//		System.out.println("Partie imaginaire de c1 = " + c1.imaginary());
+		
+
+	}
+	
+}
+	
+	
+
+
+//
+//c1.add(c2);
+//System.out.println("c1 = " + c1.get_number_toString());
+//
+//c1.set_c("Ceci est un commentaire");
+//
+//System.out.println("1 = " + c1.get_c());
+//System.out.println("2 = " + c2.get_c());
+
+
+
+
+//class CallByReference { 
+//	  
+//    int a, b; 
+//  
+//    // Function to assign the value 
+//    // to the class variables 
+//    CallByReference(int x, int y) 
+//    { 
+//        a = x; 
+//        b = y; 
+//    } 
+//  
+//    // Changing the values of class variables 
+//    void ChangeValue(CallByReference obj) 
+//    { 
+//        obj.a += 10; 
+//        obj.b += 20; 
+//    } 
+//} 
